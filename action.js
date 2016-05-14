6fbbe07cb27fa5c04d25b42bc153af627613d08a
Fredobagel <Fredobagel@users.noreply.github.com>
2016-05-14 21:48:40 +0100

    Add files via upload

---
+++ action.js
@@ -0,0 +1,13 @@
+var slideNumber = 1;
+
+$(document).ready(function(){
+	
+	$('.buttonRight').click(function() {
+		$('#pic1').addClass('hidden');
+		$('#pic1').removeClass('shown');
+		$('#pic2').addClass('shown');
+		$('pic2').removeClass('hidden');
+	});
+	
+	
+});
